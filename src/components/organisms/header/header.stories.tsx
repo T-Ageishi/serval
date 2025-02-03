dd1b61d9d2e77298d3a72ddbcde78e0730c22dad
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-04 00:00:03 +0900

    Fix routing in storybook

--- src/components/organisms/header/header.stories.tsx
+++ src/components/organisms/header/header.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Header from ".";
+import { withRouter } from "storybook-addon-remix-react-router";
 
 export default {
 	title: "Organisms/Header",
@@ -11,5 +12,6 @@ export default {
 
 type Story = StoryObj<typeof Header>;
 export const Default: Story = {
+	decorators: [withRouter],
 	args: {},
 };
