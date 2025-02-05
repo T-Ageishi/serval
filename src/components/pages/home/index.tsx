38652a8d7cd343cfabd17492ac88e7f57296fdfb
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-05 13:16:32 +0900

    Change component's name

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,3 +1,3 @@
-export function HomePage() {
+export function Home() {
 	return <div>ホーム</div>;
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router";
 import ProjectsPage from "@/components/pages/projects";
-import { HomePage } from "@/components/pages/home";
+import { Home } from "@/components/pages/home";
 import MainTemplate from "@/components/templates/main";
 
 export function AppRoutes() {
 	return (
 		<Routes>
 			<Route element={<MainTemplate />}>
-				<Route index element={<HomePage />}></Route>
+				<Route index element={<Home />}></Route>
 				<Route path={"/projects"} element={<ProjectsPage />} />
 			</Route>
 		</Routes>
