0c4bd1f74bcf21b786dddf48c19a7be7207d9d45
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-05 23:50:18 +0900

    Move states

--- src/components/organisms/projects-search/index.tsx
+++ src/components/organisms/projects-search/index.tsx
@@ -1,38 +1,58 @@
 "use client";
 
-import { ChangeEventHandler } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import Text from "@/components/atoms/text";
 import Chips, { ChipProps } from "@/components/atoms/chips";
 import styles from "./index.module.css";
+import {
+	selectKeyword,
+	updateKeyword,
+	addCategory,
+	removeCategory,
+} from "@/features/search/search-slice.ts";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "@/hooks/use-app-selector.ts";
 
-export default function ProjectsSearch({
-	keyword,
-	onChangeKeyword,
-	onChangeChips,
-}: ProjectsSearchProps) {
-	// @@todo データ取得
-	const props: ChipProps[] = [
-		{ label: "その他", value: -99999, onChange: onChangeChips },
-	];
+export default function ProjectsSearch() {
+	const [categories, setCategories] = useState<Category[]>([]);
+	const keyword = useAppSelector(selectKeyword);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		// @@todo データ取得
+		setTimeout(() => setCategories([{ value: -99999, label: "その他" }]), 5000);
+		return () => setCategories([]);
+	}, []);
+
+	const chipProps: ChipProps[] = useMemo(() => {
+		return categories.map((c) => ({ ...c, onChange: onChangeChips }));
+	}, [categories]);
 
 	return (
 		<section>
 			<Text
 				value={keyword}
-				onChange={onChangeKeyword}
+				onChange={(e) => dispatch(updateKeyword(e.target.value))}
 				className={styles["search__text"]}
 				prependIcon={"search"}
 				placeholder={"プロジェクトを検索"}
 			/>
 			<div className={styles["search__chips-container"]}>
-				<Chips chipPropsList={props} />
+				<Chips chipPropsList={chipProps} />
 			</div>
 		</section>
 	);
 }
 
-type ProjectsSearchProps = {
-	keyword: string;
-	onChangeKeyword: ChangeEventHandler<HTMLInputElement>;
-	onChangeChips: ChangeEventHandler<HTMLInputElement>;
-};
+/**
+ * カテゴリーの選択状態が変化したときの処理
+ */
+function onChangeChips(e: ChangeEvent<HTMLInputElement>) {
+	if (e.currentTarget.checked) {
+		addCategory(Number(e.currentTarget.value));
+	} else {
+		removeCategory(Number(e.currentTarget.value));
+	}
+}
+
+type Category = { value: number; label: string };
--- src/components/pages/projects/index.tsx
+++ src/components/pages/projects/index.tsx
@@ -2,35 +2,11 @@ import NewProjectCard from "@/components/molecules/new-project-card";
 import styles from "./index.module.css";
 import ProjectsSearch from "@/components/organisms/projects-search";
 import WithHeroTemplate from "@/components/templates/with-hero";
-import { ChangeEventHandler } from "react";
-import {
-	selectKeyword,
-	updateKeyword,
-	addCategory,
-	removeCategory,
-} from "@/features/search/search-slice.ts";
-import { useDispatch } from "react-redux";
-import { useAppSelector } from "@/hooks/use-app-selector.ts";
 
 export default function Projects() {
-	const keyword = useAppSelector(selectKeyword);
-	const dispatch = useDispatch();
-
-	const onChangeChips: ChangeEventHandler<HTMLInputElement> = (e) => {
-		if (e.currentTarget.checked) {
-			addCategory(Number(e.currentTarget.value));
-		} else {
-			removeCategory(Number(e.currentTarget.value));
-		}
-	};
-
 	return (
 		<WithHeroTemplate heroLabel={"プロジェクト一覧"}>
-			<ProjectsSearch
-				keyword={keyword}
-				onChangeKeyword={(e) => dispatch(updateKeyword(e.target.value))}
-				onChangeChips={onChangeChips}
-			/>
+			<ProjectsSearch />
 			<section className={styles["page__cards"]}>
 				<NewProjectCard />
 			</section>
