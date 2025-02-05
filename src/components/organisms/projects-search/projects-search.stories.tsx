e9c2f9856fc59492d3b5c06b3fb95e38c937311c
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-05 22:06:41 +0900

    Inline functions

--- src/components/organisms/projects-search/projects-search.stories.tsx
+++ src/components/organisms/projects-search/projects-search.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import ProjectsSearch from "@/components/organisms/projects-search/index";
-import { ChangeEventHandler, useState } from "react";
+import { useState } from "react";
 
 export default {
 	title: "Organisms/ProjectsSearch",
@@ -19,22 +19,18 @@ export const Default: Story = {
 function ProjectsSearchWithHook() {
 	const [keyword, setKeyword] = useState<string>("");
 	const [selectedChips, setSelectedChips] = useState<number[]>([]);
-	const onChangeKeyword: ChangeEventHandler<HTMLInputElement> = (e) => {
-		setKeyword(e.target.value);
-	};
-	const onChangeChips: ChangeEventHandler<HTMLInputElement> = (e) => {
-		const value = Number(e.currentTarget.value);
-		if (e.currentTarget.checked) {
-			setSelectedChips([...selectedChips, value]);
-		} else {
-			setSelectedChips(selectedChips.filter((v) => v !== value));
-		}
-	};
 	return (
 		<ProjectsSearch
 			keyword={keyword}
-			onChangeKeyword={onChangeKeyword}
-			onChangeChips={onChangeChips}
+			onChangeKeyword={(e) => setKeyword(e.target.value)}
+			onChangeChips={(e) => {
+				const value = Number(e.currentTarget.value);
+				if (e.currentTarget.checked) {
+					setSelectedChips([...selectedChips, value]);
+				} else {
+					setSelectedChips(selectedChips.filter((v) => v !== value));
+				}
+			}}
 		/>
 	);
 }
