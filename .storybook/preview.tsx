cab567b14f76c7f1c979114601f0310e1e06fa9a
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-06 23:53:52 +0900

    Add a theme toggle button in the header component

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,14 +2,18 @@ import type { Preview } from "@storybook/react";
 import "@/index.css";
 import styles from "./index.module.css";
 import { withRouter } from "storybook-addon-remix-react-router";
+import { Provider } from "react-redux";
+import { store } from "@/store.ts";
 
 const preview: Preview = {
 	decorators: [
 		withRouter,
 		(Story) => (
-			<div className={`${styles["container"]}`}>
-				<Story />
-			</div>
+			<Provider store={store}>
+				<div className={`${styles["container"]}`}>
+					<Story />
+				</div>
+			</Provider>
 		),
 	],
 	parameters: {
--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
 import Navigation from "@/components/molecules/navigation";
-import styles from "./index.module.css";
 import { NavLink } from "react-router";
+import Button from "@/components/molecules/button";
+import styles from "./index.module.css";
+import { useAppSelector } from "@/redux/hooks/use-app-selector.ts";
+import { darken, lighten, selectTheme } from "@/redux/slices/theme-slice.ts";
+import { useDispatch } from "react-redux";
 
 export default function Header(): ReactNode {
+	const theme = useAppSelector(selectTheme);
+	const dispatch = useDispatch();
+
 	return (
 		<header className={styles["header"]}>
 			<div className={styles["header__contents"]}>
@@ -17,6 +24,11 @@ export default function Header(): ReactNode {
 					/>
 				</NavLink>
 				<Navigation />
+				<Button
+					icon={theme === "light" ? "dark_mode" : "light_mode"}
+					className={styles["header__theme-button"]}
+					onClick={() => dispatch(theme === "light" ? darken() : lighten())}
+				/>
 			</div>
 		</header>
 	);
