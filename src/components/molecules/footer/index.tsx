024ebce969f58af459eafd67aead39d7020f1f00
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-07 13:15:05 +0900

    Edit a message

--- src/components/molecules/footer/index.tsx
+++ src/components/molecules/footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 export default function Footer() {
 	return (
 		<footer className={styles["footer"]}>
-			<span>2025 H1 MBO, powered by Next.js.</span>
+			<span>2025 H1 MBO, powered by Vite.</span>
 		</footer>
 	);
 }
