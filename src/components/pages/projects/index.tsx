cb95cc78c320946fe3d369314aaad49c9e2947b6
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-12 07:51:56 +0900

    サンプルのデータ追加

--- src/components/pages/projects/index.tsx
+++ src/components/pages/projects/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./index.module.css";
 import NewProjectCard from "@/components/molecules/new-project-card";
+import ProjectCard from "@/components/molecules/project-card";
 import ProjectsSearch from "@/components/organisms/projects-search";
 import WithHeroTemplate from "@/components/templates/with-hero";
 
@@ -9,7 +10,81 @@ export default function Projects() {
 			<ProjectsSearch />
 			<section className={styles["page__cards"]}>
 				<NewProjectCard />
+				{makeProjectCardProps().map(({ id, ...props }) => (
+					<ProjectCard key={id} {...props} />
+				))}
 			</section>
 		</WithHeroTemplate>
 	);
+}
+
+function makeProjectCardProps() {
+	return _getProjects().map((p) => {
+		return {
+			id: p.id,
+			title: p.title,
+			tags: p.tags,
+			startsAt: p.starts_at,
+			endsAt: p.ends_at,
+			tagMaster: _getTags(),
+		};
+	});
+
+	function _getProjects() {
+		return [
+			{
+				id: 1,
+				title: "サンプルプロジェクト１",
+				tags: ["1"],
+				starts_at: Date.now(),
+				ends_at: Date.now(),
+			},
+			{
+				id: 2,
+				title: "サンプルプロジェクト１",
+				tags: ["2"],
+				starts_at: Date.now(),
+				ends_at: Date.now(),
+			},
+			{
+				id: 3,
+				title: "サンプルプロジェクト２",
+				tags: ["3"],
+				starts_at: Date.now(),
+				ends_at: Date.now(),
+			},
+			{
+				id: 4,
+				title: "サンプルプロジェクト３",
+				tags: ["4"],
+				starts_at: Date.now(),
+				ends_at: Date.now(),
+			},
+			{
+				id: 5,
+				title: "サンプルプロジェクト４",
+				tags: ["5"],
+				starts_at: Date.now(),
+				ends_at: Date.now(),
+			},
+			{
+				id: 6,
+				title: "サンプルプロジェクト５",
+				tags: ["6"],
+				starts_at: Date.now(),
+				ends_at: Date.now(),
+			},
+		];
+	}
+
+	function _getTags() {
+		return [
+			{ id: "1", title: "サンプルタグ１" },
+			{ id: "2", title: "サンプルタグ２" },
+			{ id: "3", title: "サンプルタグ３" },
+			{ id: "4", title: "サンプルタグ４" },
+			{ id: "5", title: "サンプルタグ５" },
+			{ id: "6", title: "サンプルタグ６" },
+		];
+	}
 }
