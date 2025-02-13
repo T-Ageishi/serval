3c836917424b5bb42846ab9c515ced374111c582
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-13 23:42:48 +0900

    Extract project list

---
+++ src/components/organisms/projects-list/index.tsx
@@ -0,0 +1,89 @@
+import styles from "./index.module.css";
+import Skeleton from "@/components/atoms/skeleton";
+import NewProjectCard from "@/components/molecules/new-project-card";
+import ProjectCard from "@/components/molecules/project-card";
+import { useAppSelector } from "@/redux/hooks";
+import {
+	selectCategoriesError,
+	selectCategoriesStatus,
+} from "@/redux/slices/category-slice.ts";
+import {
+	Project,
+	selectProjects,
+	selectProjectsError,
+	selectProjectsStatus,
+} from "@/redux/slices/projects-slice.ts";
+import {
+	selectCategories,
+	selectKeyword,
+} from "@/redux/slices/search-slice.ts";
+import { clsx } from "clsx";
+
+export default function ProjectsList() {
+	const projectsStatus = useAppSelector(selectProjectsStatus);
+	const projects = useAppSelector(selectProjects);
+	const projectsError = useAppSelector(selectProjectsError);
+	const categoriesStatus = useAppSelector(selectCategoriesStatus);
+	const categoriesError = useAppSelector(selectCategoriesError);
+
+	//検索条件
+	const keyword = useAppSelector(selectKeyword);
+	const categories = useAppSelector(selectCategories);
+
+	//データ取得完了
+	if (projectsStatus === "succeeded" && categoriesStatus === "succeeded") {
+		return (
+			<section className={styles["list"]}>
+				<NewProjectCard />
+				{projects.filter(createFilter(keyword, categories)).map((p) => (
+					<ProjectCard key={p.id} {...makeProjectCardProps(p)} />
+				))}
+			</section>
+		);
+	}
+
+	//データ取得失敗
+	if (projectsStatus === "failed" || categoriesStatus === "failed") {
+		return (
+			<section className={clsx([styles["list"], styles["list--error"]])}>
+				<p>データ取得中にエラーが発生しました。</p>
+				{projectsError !== null && <p>{projectsError}</p>}
+				{categoriesError !== null && <p>{categoriesError}</p>}
+			</section>
+		);
+	}
+
+	//ローディング中
+	return (
+		<section className={styles["list"]}>
+			<Skeleton height={"auto"} width={"auto"} />
+			<Skeleton height={"auto"} width={"auto"} />
+			<Skeleton height={"auto"} width={"auto"} />
+		</section>
+	);
+}
+
+function createFilter(keyword: string, categories: Project["tags"]) {
+	return (project: Project): boolean => {
+		let ret = true;
+		if (keyword !== "" && !project.title.includes(keyword)) {
+			ret = false;
+		}
+		if (
+			categories.length > 0 &&
+			!project.tags.some((t) => categories.includes(t))
+		) {
+			ret = false;
+		}
+		return ret;
+	};
+}
+
+function makeProjectCardProps(project: Project) {
+	return {
+		title: project.title,
+		tags: project.tags,
+		startsAt: project.starts_at,
+		endsAt: project.ends_at,
+	};
+}
--- src/components/pages/projects/index.tsx
+++ src/components/pages/projects/index.tsx
@@ -1,8 +1,5 @@
-import styles from "./index.module.css";
 import Hero from "@/components/atoms/hero";
-import Skeleton from "@/components/atoms/skeleton";
-import NewProjectCard from "@/components/molecules/new-project-card";
-import ProjectCard from "@/components/molecules/project-card";
+import ProjectsList from "@/components/organisms/projects-list";
 import ProjectsSearch from "@/components/organisms/projects-search";
 import Stack from "@/components/templates/stack";
 import { useAppSelector } from "@/redux/hooks";
@@ -12,25 +9,14 @@ import {
 } from "@/redux/slices/category-slice.ts";
 import {
 	fetchProjects,
-	Project,
-	selectProjectsError,
-	selectProjects,
 	selectProjectsStatus,
 } from "@/redux/slices/projects-slice.ts";
-import {
-	selectCategories,
-	selectKeyword,
-} from "@/redux/slices/search-slice.ts";
 import { store } from "@/store.ts";
 import { useEffect } from "react";
 
 export default function Projects() {
 	const projectsStatus = useAppSelector(selectProjectsStatus);
-	const projects = useAppSelector(selectProjects);
-	const error = useAppSelector(selectProjectsError);
 	const categoriesStatus = useAppSelector(selectCategoriesStatus);
-	const keyword = useAppSelector(selectKeyword);
-	const categories = useAppSelector(selectCategories);
 
 	useEffect(() => {
 		if (projectsStatus === "idle") {
@@ -45,54 +31,7 @@ export default function Projects() {
 		<Stack>
 			<Hero children={"プロジェクト一覧"} />
 			<ProjectsSearch />
-			<section className={styles["page__cards"]}>
-				{projectsStatus === "succeeded" && categoriesStatus === "succeeded" ? (
-					<>
-						<NewProjectCard />
-						{projects.filter(createFilter(keyword, categories)).map((p) => (
-							<ProjectCard key={p.id} {...makeProjectCardProps(p)} />
-						))}
-					</>
-				) : projectsStatus === "failed" || categoriesStatus === "failed" ? (
-					<div>
-						<p>データ取得中にエラーが発生しました。</p>
-						{error !== null && <p>{error}</p>}
-					</div>
-				) : (
-					<>
-						<Skeleton height={"auto"} width={"auto"} />
-						<Skeleton height={"auto"} width={"auto"} />
-						<Skeleton height={"auto"} width={"auto"} />
-						<Skeleton height={"auto"} width={"auto"} />
-						<Skeleton height={"auto"} width={"auto"} />
-					</>
-				)}
-			</section>
+			<ProjectsList />
 		</Stack>
 	);
-}
-
-function createFilter(keyword: string, categories: Project["tags"]) {
-	return (project: Project): boolean => {
-		let ret = true;
-		if (keyword !== "" && !project.title.includes(keyword)) {
-			ret = false;
-		}
-		if (
-			categories.length > 0 &&
-			!project.tags.some((t) => categories.includes(t))
-		) {
-			ret = false;
-		}
-		return ret;
-	};
-}
-
-function makeProjectCardProps(project: Project) {
-	return {
-		title: project.title,
-		tags: project.tags,
-		startsAt: project.starts_at,
-		endsAt: project.ends_at,
-	};
 }
