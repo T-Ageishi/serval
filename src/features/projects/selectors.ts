7edccaa8b181786214e06021a35e2522008e79d2
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-14 21:00:06 +0900

    Rearrange code

--- src/features/projects/selectors.ts
+++ src/features/projects/selectors.ts
@@ -1,6 +1,6 @@
 import { RootState } from "@/app/store.ts";
 
 export const selectProjects = (state: RootState) => state.projects.projects;
+export const selectCategories = (state: RootState) => state.category.items;
 export const selectProjectsStatus = (state: RootState) => state.projects.status;
 export const selectProjectsError = (state: RootState) => state.projects.error;
-export const selectCategories = (state: RootState) => state.category.items;
