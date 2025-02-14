ff01e1b869ebde2eca0acd1ba5478eb5b2953f56
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-02-14 20:40:21 +0900

    Add category type

--- src/features/projects/types.ts
+++ src/features/projects/types.ts
@@ -1,14 +1,19 @@
-import { Category } from "@/redux/slices/category-slice.ts";
-
 export type ProjectsState = {
 	items: Project[];
+	categories?: Category[]; //@@todo ?外す
 	status: "idle" | "pending" | "succeeded" | "failed";
 	error: string | null;
 };
+
 export type Project = {
 	id: number;
 	title: string;
 	tags: Category["id"][];
 	starts_at: number;
 	ends_at: number;
+};
+
+type Category = {
+	id: number;
+	title: string;
 };
